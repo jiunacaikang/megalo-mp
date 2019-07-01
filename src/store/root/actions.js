b50c90e4225d0582493c9ceabb9ff1811dfb457c
caikangkang <caikangkang@migu.cn>
2019-07-01 17:06:43 +0800

    other# 修改action

--- src/store/root/actions.js
+++ src/store/root/actions.js
@@ -1,10 +1,11 @@
 import INTERFACE from "@/api/interface"
+import * as TYPES from "../mutation-types";
 
 const actions = {
     async initData({commit}) {
         const res = await INTERFACE.initData();
         if(res.code == 200){
-            commit(res)
+            commit(TYPES.INIT_DATA,res)
         }
         return res;
     },
